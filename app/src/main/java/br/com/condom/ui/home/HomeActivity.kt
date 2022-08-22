97ce09f2dde94695c3522aa87d0f54f75b85cec9
Renato Santos <renato.lucass@live.com>
2022-08-22 12:42:44 -0300

    Home e Login, precisa ajustar o layout

---
+++ app/src/main/java/br/com/condom/ui/home/HomeActivity.kt
@@ -0,0 +1,40 @@
+package br.com.condom.ui.home
+
+import android.os.Bundle
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import br.com.condom.R
+import br.com.condom.databinding.ActivityHomeBinding
+import br.com.condom.ui.login.LoginViewModel
+import android.content.Intent
+import br.com.condom.ui.login.LoginActivity
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var homeViewModel: HomeViewModel
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
+        binding.signin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/br/com/condom/ui/home/HomeViewModel.kt
@@ -0,0 +1,16 @@
+package br.com.condom.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import android.util.Patterns
+import br.com.condom.data.LoginRepository
+import br.com.condom.data.Result
+
+import br.com.condom.R
+
+class HomeViewModel(private val loginRepository: LoginRepository) : ViewModel() {
+
+
+
+}
